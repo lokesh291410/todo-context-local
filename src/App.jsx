5ada9b9f709096add222e68f14bb7a8daec5000b
Lokesh Kumar <lokesh.work2910@gmail.com>
2024-05-24 12:04:10 +0530

    added todo form and items ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoProvider } from './context'
+import TodoForm from './components/TodoFrom'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, settodos] = useState([])
@@ -21,7 +21,7 @@ function App() {
 
   const toggleComplete = (id) => {
     settodos((prev) => prev.map(
-      prevTodo => prevTodo.id === id ? {...prevTodo, complete: !prevTodo.complete} : prevTodo
+      prevTodo => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo
     ))
   }
 
@@ -41,9 +41,15 @@ function App() {
                 <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                 <div className="mb-4">
                     {/* Todo form goes here */} 
+                    <TodoForm />
                 </div>
                 <div className="flex flex-wrap gap-y-3">
                     {/*Loop and Add TodoItem here */}
+                    {todos.map((todo => (
+                      <div key={todo.id} className='w-full'>
+                        <TodoItem todo={todo} />
+                      </div>
+                    )))}
                 </div>
             </div>
         </div>
